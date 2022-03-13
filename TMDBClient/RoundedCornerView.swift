ce67676c2c4b2a6d36add76d951302dbacaf9614
Chris <christian.molnar@playermaker.com>
2022-03-13 01:28:04 +0100

    Added basic Animations without clearing

--- TMDBClient/RoundedCornerView.swift
+++ TMDBClient/RoundedCornerView.swift
@@ -10,27 +10,26 @@ import UIKit
 @IBDesignable
 class RoundedCornerView: UIView {
 
-    @IBInspectable var leftTopRadius : CGFloat = 0 {
-        didSet{
+    @IBInspectable var leftTopRadius: CGFloat = 0 {
+        didSet {
             self.applyMask()
         }
     }
-    @IBInspectable var rightTopRadius : CGFloat = 0 {
-        didSet{
+    @IBInspectable var rightTopRadius: CGFloat = 0 {
+        didSet {
             self.applyMask()
         }
     }
-    @IBInspectable var rightBottomRadius : CGFloat = 0 {
-        didSet{
+    @IBInspectable var rightBottomRadius: CGFloat = 0 {
+        didSet {
             self.applyMask()
         }
     }
-    @IBInspectable var leftBottomRadius : CGFloat = 0 {
-        didSet{
+    @IBInspectable var leftBottomRadius: CGFloat = 0 {
+        didSet {
             self.applyMask()
         }
     }
-
 
     override func layoutSubviews() {
         super.layoutSubviews()
@@ -45,30 +44,42 @@ class RoundedCornerView: UIView {
     }
     */
 
-    func applyMask()
-    {
+    func applyMask() {
         let shapeLayer = CAShapeLayer(layer: self.layer)
-        shapeLayer.path = self.pathForCornersRounded(rect:self.bounds).cgPath
+        shapeLayer.path = self.pathForCornersRounded(rect: self.bounds).cgPath
         shapeLayer.frame = self.bounds
         shapeLayer.masksToBounds = true
         self.layer.mask = shapeLayer
     }
 
-    func pathForCornersRounded(rect:CGRect) -> UIBezierPath
-    {
+    func pathForCornersRounded(rect: CGRect) -> UIBezierPath {
         let path = UIBezierPath()
-        path.move(to: CGPoint(x: 0 + leftTopRadius , y: 0))
-        path.addLine(to: CGPoint(x: rect.size.width - rightTopRadius , y: 0))
-        path.addQuadCurve(to: CGPoint(x: rect.size.width , y: rightTopRadius), controlPoint: CGPoint(x: rect.size.width, y: 0))
-        path.addLine(to: CGPoint(x: rect.size.width , y: rect.size.height - rightBottomRadius))
-        path.addQuadCurve(to: CGPoint(x: rect.size.width - rightBottomRadius , y: rect.size.height), controlPoint: CGPoint(x: rect.size.width, y: rect.size.height))
-        path.addLine(to: CGPoint(x: leftBottomRadius , y: rect.size.height))
-        path.addQuadCurve(to: CGPoint(x: 0 , y: rect.size.height - leftBottomRadius), controlPoint: CGPoint(x: 0, y: rect.size.height))
-        path.addLine(to: CGPoint(x: 0 , y: leftTopRadius))
-        path.addQuadCurve(to: CGPoint(x: 0 + leftTopRadius , y: 0), controlPoint: CGPoint(x: 0, y: 0))
+        path.move(to: CGPoint(x: 0 + leftTopRadius, y: 0))
+        path.addLine(to: CGPoint(x: rect.size.width - rightTopRadius, y: 0))
+        path.addQuadCurve(to: CGPoint(x: rect.size.width, y: rightTopRadius), controlPoint: CGPoint(x: rect.size.width, y: 0))
+        path.addLine(to: CGPoint(x: rect.size.width, y: rect.size.height - rightBottomRadius))
+        path.addQuadCurve(to: CGPoint(x: rect.size.width - rightBottomRadius, y: rect.size.height), controlPoint: CGPoint(x: rect.size.width, y: rect.size.height))
+        path.addLine(to: CGPoint(x: leftBottomRadius, y: rect.size.height))
+        path.addQuadCurve(to: CGPoint(x: 0, y: rect.size.height - leftBottomRadius), controlPoint: CGPoint(x: 0, y: rect.size.height))
+        path.addLine(to: CGPoint(x: 0, y: leftTopRadius))
+        path.addQuadCurve(to: CGPoint(x: 0 + leftTopRadius, y: 0), controlPoint: CGPoint(x: 0, y: 0))
         path.close()
 
         return path
     }
 
+}
+
+@IBDesignable
+class BorderTextField: UITextField {
+ @IBInspectable var borderColor: UIColor? {
+    didSet {
+        layer.borderColor = borderColor?.cgColor
+    }
+ }
+ @IBInspectable var borderWidth: CGFloat = 0 {
+    didSet {
+        layer.borderWidth = borderWidth
+    }
+ }
 }
--- TMDBClient/ViewController.swift
+++ TMDBClient/ViewController.swift
@@ -9,9 +9,91 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainView: RoundedCornerView!
+    @IBOutlet weak var robotHeight: NSLayoutConstraint!
+    @IBOutlet weak var searchBlockHeight: NSLayoutConstraint!
+    @IBOutlet weak var robotIcon: UIImageView!
+    @IBOutlet weak var searchTextFieldTrailing: NSLayoutConstraint!
+    @IBOutlet weak var searchTextFieldLeading: NSLayoutConstraint!
+    @IBOutlet weak var robotIcoTop: NSLayoutConstraint!
+    @IBOutlet weak var robotIcoLeading: NSLayoutConstraint!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var mainViewBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+
+    private var isCollapsed: Bool = false
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(handle(keyboardShowNotification:)),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
     }
 
+    @IBAction func didTapSubmitButton(_ sender: Any) {
+        if let text = self.searchTextField.text {
+            SearchAPI().performSearchRequest(with: text)
+                .onComplete { _ in
+                    self.isCollapsed = true
+                    self.searchTextField.endEditing(true)
+                    self.submitButton.isHidden = true
+                    UIView.animate(withDuration: 0.3) {
+                        self.mainView.leftBottomRadius = 25.0
+                        self.mainView.rightBottomRadius = 25.0
+                        self.robotIcoTop.constant = 35.0
+                        self.robotIcoLeading.constant = 24.0
+                        self.robotHeight.constant = 50.0
+                        self.searchTextFieldTrailing.constant = 24.0
+                        self.searchTextFieldLeading.constant = self.robotIcoLeading.constant + 100.0
+                        self.searchBlockHeight.constant = 55
+                        self.mainViewBottomConstraint.constant = self.view.frame.height - 100
+                        self.view.layoutIfNeeded()
+                    }
+                }
+        }
+    }
+
+    @objc
+    private func handle(keyboardShowNotification notification: Notification) {
+        if !isCollapsed, let userInfo = notification.userInfo,
+            let keyboardRectangle = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
+            UIView.animate(withDuration: 0.3) {
+                [self.titleLabel, self.subtitleLabel].forEach { label in
+                    label?.layer.opacity = 0
+                    label?.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+                }
+                self.mainViewBottomConstraint.constant += keyboardRectangle.height
+                self.robotHeight.constant /= 1.5
+                self.view.layoutIfNeeded()
+            } completion: { _ in
+                [self.titleLabel, self.subtitleLabel].forEach { label in
+                    label?.isHidden = true
+                }
+            }
+        }
+    }
+
+    @IBAction func didChangedValue(_ sender: Any) {
+        guard let text = searchTextField.text else { return }
+        if !isCollapsed {
+            self.submitButton.isEnabled = text.isEmpty ? false : true
+            self.submitButton.backgroundColor = UIColor(named: text.isEmpty ? "base-app-orange-inactive" : "base-app-orange-active")
+        } else {
+            SearchAPI().performSearchRequest(with: text)
+                .onComplete { _ in
+
+                }
+        }
+
+    }
+}
+
+extension ViewController {
+    struct Constants {
+        static let mainViewBottomInset: CGFloat = 78.0
+    }
 }
